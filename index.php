65ffdce77f0c6e3d858a2201324af1fc568297df
Ziga Krasovec <zkrasovec@MacBook-Air.local>
2016-03-04 17:24:15 +0100

    Added jumbtron and two main sections

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <!DOCTYPE html>
 <html>
 <head>
+  <!-- Charset Definition -->
+  <meta charset="UTF-8">
+  
   <!-- Viewport Settings -->
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
   
@@ -11,28 +14,74 @@
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
   
   <!-- Custom Style -->
-  <link rel="stylesheet" type="text/css" href="main.css">
+  <link rel="stylesheet" type="text/css" href="CSS/main.css">
 </head>
 <body>
 <header>
   <div class="container">
     <div class="row">
-      <div id="logo" class="col-md-4"></div><!-- END logo -->
-      <nav id="main-nav" class="col-md-8">
-        <ul>
-          <li></li>
+      <div id="logo" class="col-sm-1">
+        <a href="index.php">
+          <img src="img/logo.svg" id="logo-image">
+        </a>
+      </div><!-- END logo -->
+      <p class="col-sm-3">
+        Žiga Krašovec
+      </p><!-- END header paragraph -->
+      <nav id="main-nav" class="col-sm-8">
+        <ul class="text-right">
+          <li><a href="#">Domov</a></li>
+          <li><a href="#">Storitve</a></li>
+          <li><a href="#">Kontakt</a></li>
+          <li><a href="#">Blog</a></li>
         </ul>
       </nav><!-- END main nav -->
     </div><!-- END row -->
   </div><!-- END container -->
-</header>
+</header><!-- END header -->
+  
+<div class="jumbotron" id="main-jt">
+  <h1>Dobrodošli na moji spletni strani!</h1>
+  <button href="#" class="btn btn-success" id="poglej-vec">Poglej več</button>
+  <span id="show-content" class="glyphicon glyphicon-chevron-down" aria-hidden="true"></span>
+</div><!-- END jumbotron -->
+  
+<section class="container" id="firstSection">
+  <h2>Na kratko o meni</h2>
+  <hr>
+  <div class="row">
+    <img src="img/avatar.jpg" class="img-circle col-sm-3">
+    <p class="col-sm-9">Gentrify seitan selfies, kickstarter everyday carry photo booth twee thundercats food truck actually meggings heirloom put a bird on it godard. Narwhal cold-pressed keffiyeh green juice skateboard. You probably haven't heard of them echo park aesthetic ramps. Direct trade kogi YOLO, blog leggings literally kale chips disrupt typewriter mustache mumblecore iPhone bespoke selvage. Godard lumbersexual cardigan asymmetrical. Occupy mixtape man bun typewriter plaid fashion axe flexitarian chartreuse brooklyn, pork belly wayfarers portland quinoa aesthetic. Roof party scenester neutra small batch, kombucha tofu yr mumblecore tote bag sustainable taxidermy.
+
+Blog umami neutra, chartreuse pickled +1 blue bottle cronut lomo literally gastropub. Trust fund 8-bit try-hard cred. Schlitz VHS letterpress, austin flexitarian DIY tousled pickled kickstarter meggings godard pinterest franzen. Chillwave bicycle rights blog, occupy franzen affogato whatever bespoke messenger bag pinterest fashion axe marfa church-key keytar post-ironic. Brooklyn letterpress franzen, actually kitsch mumblecore fixie poutine polaroid. Leggings readymade messenger bag wayfarers, chia man bun lumbersexual pinterest biodiesel roof party fixie everyday carry PBR&B. Brooklyn single-origin coffee viral affogato lo-fi, cray twee photo booth intelligentsia four dollar toast readymade polaroid ugh taxidermy wayfarers.</p>
+  </div><!-- END row-->
+</section><!-- END firstSection -->
+  
+<div class="jumbotron jt-lvl2" id="jt-2">
+  <h2>Moje dejavnosti</h2>
+</div>  
+  
+<section class="container" id="secondSection">
+  <h2>S čim se ukvarjam</h2>
+  <hr>
+  <div class="row">
+    <img src="img/dejavnost.png" class="img-circle col-sm-3 col-sm-push-9">
+    <p class="col-sm-9 col-sm-pull-3">Gentrify seitan selfies, kickstarter everyday carry photo booth twee thundercats food truck actually meggings heirloom put a bird on it godard. Narwhal cold-pressed keffiyeh green juice skateboard. You probably haven't heard of them echo park aesthetic ramps. Direct trade kogi YOLO, blog leggings literally kale chips disrupt typewriter mustache mumblecore iPhone bespoke selvage. Godard lumbersexual cardigan asymmetrical. Occupy mixtape man bun typewriter plaid fashion axe flexitarian chartreuse brooklyn, pork belly wayfarers portland quinoa aesthetic. Roof party scenester neutra small batch, kombucha tofu yr mumblecore tote bag sustainable taxidermy.
+
+Blog umami neutra, chartreuse pickled +1 blue bottle cronut lomo literally gastropub. Trust fund 8-bit try-hard cred. Schlitz VHS letterpress, austin flexitarian DIY tousled pickled kickstarter meggings godard pinterest franzen. Chillwave bicycle rights blog, occupy franzen affogato whatever bespoke messenger bag pinterest fashion axe marfa church-key keytar post-ironic. Brooklyn letterpress franzen, actually kitsch mumblecore fixie poutine polaroid. Leggings readymade messenger bag wayfarers, chia man bun lumbersexual pinterest biodiesel roof party fixie everyday carry PBR&B. Brooklyn single-origin coffee viral affogato lo-fi, cray twee photo booth intelligentsia four dollar toast readymade polaroid ugh taxidermy wayfarers.</p>
+  </div><!-- END row-->
+</section><!-- END firstSection -->
   
   
   
   
   
   
+<!-- Minified jQuery -->
+<script src="JS/jquery.min.js" type="text/javascript"></script>
   
+<!-- Scroll To JS -->
+<script src="JS/jquery.scrollTo.min.js"></script>
   
 <!-- Latest compiled and minified JavaScript -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
