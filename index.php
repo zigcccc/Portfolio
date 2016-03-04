4b7515b0826c2ba9a17a4de38c5d832dc1bdb5fb
Ziga Krasovec <zkrasovec@MacBook-Air.local>
2016-03-04 12:56:40 +0100

    CSS, JS and Basic HTML setup

--- index.php
+++ index.php
@@ -1,6 +1,43 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <link rel="stylesheet" type="text/css" href="main.css"
+  <!-- Viewport Settings -->
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+  
+  <!-- Latest compiled and minified CSS -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+  <!-- Optional theme -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+  
+  <!-- Custom Style -->
+  <link rel="stylesheet" type="text/css" href="main.css">
 </head>
+<body>
+<header>
+  <div class="container">
+    <div class="row">
+      <div id="logo" class="col-md-4"></div><!-- END logo -->
+      <nav id="main-nav" class="col-md-8">
+        <ul>
+          <li></li>
+        </ul>
+      </nav><!-- END main nav -->
+    </div><!-- END row -->
+  </div><!-- END container -->
+</header>
+  
+  
+  
+  
+  
+  
+  
+  
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+  
+<!-- Custom JS script -->
+<script src="JS/main-script.js" type="text/javascript"></script>
+</body> 
 </html>
